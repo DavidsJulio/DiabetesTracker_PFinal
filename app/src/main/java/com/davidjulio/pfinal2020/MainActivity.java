dbe13f12ed77022001183527f090d8ac11690f2d
David André Soares Júlio <davidsjulio97@gmail.com>
2020-08-16 23:37:57 +0100

    configure the app intro ( credit: https://github.com/heinrichreimer/material-intro ) create the intro_.xml (missing the image)

--- app/src/main/java/com/davidjulio/pfinal2020/MainActivity.java
+++ app/src/main/java/com/davidjulio/pfinal2020/MainActivity.java
@@ -4,11 +4,39 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.heinrichreimersoftware.materialintro.app.IntroActivity;
+import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
+
+public class MainActivity extends IntroActivity { //mudar de AppCompatActivity para extender IntroActivity
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+
+        setButtonBackVisible(false);
+        setButtonNextVisible(false);
+
+        addSlide(new FragmentSlide.Builder()
+                .background(R.color.colorIntro)
+                .fragment(R.layout.intro_1)
+                .build());
+
+        addSlide(new FragmentSlide.Builder()
+                .background(R.color.colorIntro)
+                .fragment(R.layout.intro_2)
+                .build());
+
+        addSlide(new FragmentSlide.Builder()
+                .background(R.color.colorIntro)
+                .fragment(R.layout.intro_3)
+                .build());
+
+        addSlide(new FragmentSlide.Builder()
+                .background(R.color.colorIntro)
+                .fragment(R.layout.intro_registo)
+                .canGoForward(false)
+                .build());
+
     }
 }
