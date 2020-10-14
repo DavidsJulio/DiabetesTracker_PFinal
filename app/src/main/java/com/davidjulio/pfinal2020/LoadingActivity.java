77e6a04ac36fa3a8d99bb0cf3a7823d56bc254a1
DavidsJulio <davidsjulio97@gmail.com>
2020-10-14 12:35:47 +0100

    - Creating the loading activity - Adding logo's png and get them in place (app logo and loanding activity) - Minor changes in the meals fragment (fragment_refeicoes.xml), minor changes in the strings.xml - Changing the launcher activity in the manifest

---
+++ app/src/main/java/com/davidjulio/pfinal2020/LoadingActivity.java
@@ -0,0 +1,29 @@
+package com.davidjulio.pfinal2020;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.davidjulio.pfinal2020.activity.MainActivity;
+
+public class LoadingActivity extends AppCompatActivity {
+
+    public static int SPLASH_TIME = 1500; //wait
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(LoadingActivity.this, MainActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        }, SPLASH_TIME);
+    }
+}
